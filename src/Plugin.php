21c3a1f083f087ee881882b43a2b857439843b64
Francisco Barrento <francisco.barrento@gmail.com>
2020-06-27 15:48:08 +0100

    Updating Plugin

--- src/Plugin.php
+++ src/Plugin.php
@@ -11,6 +11,9 @@ class Plugin implements PluginInterface
   public function activate(Composer $composer, IOInterface $io)
   {
 
+    $installer = new Installer($io, $composer);
+    $composer->getInstallationManager()->addInstaller($installer);
+
   }
 
 
